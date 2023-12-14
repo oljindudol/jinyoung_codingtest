92184ba5835a94f0435abceefc24b96a31b99cda
oljindudol <82567430+oljindudol@users.noreply.github.com>
2023-12-14 22:46:03 +0900

    Time: 394 ms (9.91%), Space: 154.8 MB (13.11%) - LeetHub

---
+++ 0077-combinations/0077-combinations.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+void pushdata(int _n, int _k, int depth, int from , vector<int> tmp, vector<vector<int>>& _out)
+{
+    if (depth == _k)
+    {
+        _out.push_back(tmp);
+        return;
+    }
+    int to = _n - (_k - depth) +1;
+    for (int i = from; i < to; ++i)
+    {
+        vector<int> tmptmp = tmp;
+        tmptmp.push_back(i + 1);
+        pushdata(_n, _k, depth+1, i+1, tmptmp, _out);
+
+    }
+}
+    
+    
+    vector<vector<int>> combine(int n, int k) {
+        vector<int> tmp;
+        vector<vector<int>> ret;
+        
+        pushdata(n,k, 0,0,tmp, ret);
+        
+        return ret;
+    }
+};
