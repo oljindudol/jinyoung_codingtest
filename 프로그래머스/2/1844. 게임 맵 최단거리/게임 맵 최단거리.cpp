a04da662e75d9c433fb4221e58758654f19e3c5a
oljindudol <82567430+oljindudol@users.noreply.github.com>
2024-10-10 10:51:14 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 0.32 ms, Memory: 4.11 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
@@ -1,117 +1,91 @@
 #include<vector>
-#include<queue>
+#include <queue>
+#include <iostream>
 
 using namespace std;
 
-int sx[] = {1,-1,0,0};
-int sy[] = {0,0,1,-1};
-int maxx;
-int maxy;
-int minroot;
-vector<vector<bool>> visited;
+int maxrow;
+int maxcol;
 
-//유효성검사
-bool valid(int x,int y)
+bool IsValid(int row,int col,const vector<vector<int> >& maps)
 {
-    return false == (x>= maxx || x<0 || y>=maxy||y<0);
-}
-
-void updateMinRoot(int _in)
-{
-    if( -1 == minroot)
+    if(0> row || maxrow<= row)
     {
-        minroot = _in;
-        return;
+        return false;
     }
-    if( -1 != _in)
-    {
-        minroot = min(minroot,_in);
-    }
-    else
-    {
-        return;
-    }
-}
-
-
-// int dfs(vector<vector<int>>& maps, int x,int y,int depth)
-// {
-//     //벽과만났을때
-//     if(0 == maps[x][y])
-//     {
-//         return -1;
-//     }
-//     //visited에 만났을때
-//     if(true == visited[x][y])
-//         return -1;
-//     if(x== (maxx-1) &&y== (maxy-1))
-//         return depth;
     
+    if(0>col || maxcol <= col)
+    {
+        return false;
+    }
     
-//     for(int i = 0 ; i<4;++i)
-//     {
-//         int x = sx[i];
-//         int y = sy[i];
-//         if(valid(x,y))
-//         {
-//             visited[x][y]=true;
-//                  updateMinRoot(dfs(maps,x,y,depth+1));
-//             visited[x][y]=false;
-            
-//         }
-//     }
-// }
+    if(0 == maps[row][col])
+    {
+        return false;
+    }
+    
+    return true;
+}
 
 struct node
 {
-    int x;
-    int y;
     int depth;
+    int row;
+    int col;
 };
+
+int drow[4]= {0,0,1,-1};
+int dcol[4]={1,-1,0,0};
+
+
 
 int solution(vector<vector<int> > maps)
 {
-    int answer = 10001;
-    
-    maxx = maps.size();
-    maxy = maps[0].size();
-    
-    for(int i = 0 ; i<maxx; ++i)
-    {
-        visited.push_back(vector<bool>(maxy,false));
-    }
-    
+    int answer = -1;
+    maxrow = maps.size();
+    maxcol = maps[0].size();
     queue<node> q;
-    int tarx = maxx-1;
-    int tary = maxy-1; 
     
-    q.push({0,0,1});
+    // for(int row =0; row<maxrow;++row)
+    // {
+    //     for(int col=0;col<maxcol;++col)
+    //     {
+    
+    q.push({1,0,0});
     
     while(false == q.empty())
     {
-        node curnode = q.front();
+        auto curnode = q.front();
         q.pop();
+        int currow = curnode.row;
+        int curcol = curnode.col;
         
-        if(tarx == curnode.x && tary == curnode.y)
+        if(false == IsValid(currow,curcol,maps))
         {
-            answer = min(answer,curnode.depth);
             continue;
         }
-        for(int i =0; i<4;++i)
+        maps[currow][curcol]=0;
+        
+        
+        if(currow ==maxrow-1 && curcol == maxcol-1)
         {
-            int newx = curnode.x+ sx[i];
-            int newy = curnode.y+sy[i];
-            
-            if( true == valid(newx,newy) && 1== maps[newx][newy] && false == visited[newx][newy])
-            {
-                visited[newx][newy]=true;
-                q.push({newx,newy,curnode.depth +1 });
-            }
+            answer = curnode.depth;
+            break;
+        }
+        
+        for(int i = 0;i<4;++i)
+        {
+            int newrow = currow+ drow[i];
+            int newcol = curcol + dcol[i];
+            q.push({curnode.depth+1,newrow,newcol});
         }
     }
-    
-    if(10001 == answer)
-        return -1;
+
+            
+            
+            
+    //     }
+    // }
     
     return answer;
 }
