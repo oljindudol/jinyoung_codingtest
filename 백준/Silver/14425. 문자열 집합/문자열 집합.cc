4c19c618c1484a478c26caf16afca55dd09247a0
oljindudol <82567430+oljindudol@users.noreply.github.com>
2024-10-18 14:02:58 +0900

    [Silver IV] Title: 문자열 집합, Time: 1432 ms, Memory: 1091020 KB -BaekjoonHub

---
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.cc
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <set>
+
+using namespace std;
+
+constexpr int MAXSZ = 'z' - 'a';
+constexpr int MAXLEN = 501;
+
+struct Trie
+{
+	Trie* child[MAXSZ] = { nullptr };
+	bool finish = false;
+
+	void insert(char* str)
+	{
+		if (NULL == *str)
+		{
+			finish = true;
+			return;
+		}
+
+		int curidx = *str - 'a';
+		if (nullptr == child[curidx])
+		{
+			child[curidx] = new Trie;
+		}
+		char* newstr = str + 1;
+		child[curidx]->insert(newstr);
+	}
+
+	bool find(char* str)
+	{
+		if (NULL == *str)
+		{
+			if (true == finish)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+
+		int curidx = *str - 'a';
+		if (nullptr == child[curidx])
+		{
+			return false;
+		}
+		char* newstr = str + 1;
+		return child[curidx]->find(newstr);
+	}
+};
+
+int main()
+{
+	int n, m;
+
+	cin >> n >> m;
+
+	Trie* root = new Trie;
+
+	char buffer[MAXLEN];
+
+	for (int i = 0; i < n; ++i)
+	{
+		cin >> buffer;
+		root->insert(buffer);
+	}
+
+
+	int ret = 0;
+	for (int i = 0; i < m; ++i)
+	{
+		cin >> buffer;
+		if (true == root->find(buffer))
+		{
+			++ret;
+		}
+	}
+
+	cout << ret;
+
+
+	return 0;
+}
