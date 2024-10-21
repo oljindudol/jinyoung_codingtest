eae74b113908fa8c1387d7183cba0243a0d44956
oljindudol <82567430+oljindudol@users.noreply.github.com>
2024-10-21 09:54:46 +0900

    [level 3] Title: 정수 삼각형, Time: 1.28 ms, Memory: 8.93 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.cpp
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.cpp
@@ -4,40 +4,59 @@
 
 using namespace std;
 
+int maxel=-1;
+vector<int> dp;
+int maxdepth;
+
+void dfs(const vector<vector<int>>& triangle,int depth)
+{
+    if(maxdepth < depth)
+    {
+        return;
+    }
+    
+    vector<int> pdp;
+    for(int i = 0 ; i < depth;++i)
+    {
+        pdp.push_back(dp[i]);
+    }
+    
+    //depth만큼돈다
+    for(int i = 0 ; i < depth;++i)
+    {
+        if(0 == i)
+        {
+            dp[i] = pdp[i] + triangle[depth-1][i];
+            continue;
+        }
+        else if(depth-1 == i)
+        {
+            dp[i] = pdp[i-1] +triangle[depth-1][i]; 
+            continue;
+        }
+        else
+        {
+            dp[i] = max(pdp[i] + triangle[depth-1][i] ,pdp[i-1] +triangle[depth-1][i] );
+            continue;
+        }
+    }
+    
+    dfs(triangle,depth+1);
+    
+}
+
 int solution(vector<vector<int>> triangle) {
     int answer = 0;
-    vector<int> dp;
-    dp.push_back(triangle[0][0]);
+    maxdepth = triangle.size();
     
-    for(int i =1 ;i<triangle.size();++i)
+    
+    dp = vector<int>(maxdepth,0);
+    dp[0] = triangle[0][0];
+    dfs(triangle,2);
+    
+    for(int i = 0 ; i < maxdepth; ++i)
     {
-        int sz = triangle[i].size();
-        vector<int> pdp = dp;
-        
-        for(int j = 0; j<sz;++j)
-        {
-            if(0==j)
-            {
-                dp[j] = pdp[j] +triangle[i][j];
-                continue;
-            }
-            if(sz-1 == j)
-            {
-                dp.push_back(pdp[j-1]+ triangle[i][j]);
-            }
-            else
-            {
-                dp[j] = triangle[i][j]  + max(pdp[j],pdp[j-1]);
-            }
-        }
-        
+        answer = max(answer,dp[i]);
     }
-    
-    
-    for(auto& e: dp)
-    {
-        answer = max(answer ,e);
-    }
-    
     return answer;
 }
