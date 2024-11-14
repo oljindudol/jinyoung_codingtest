009d6a106093848d85995388f58eca80c0390934
oljindudol <82567430+oljindudol@users.noreply.github.com>
2024-11-14 15:33:09 +0900

    [Bronze III] Title: 최소, 최대, Time: 104 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.cc
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <set>
+#include <cmath>
+#include <string>
+#include <stdio.h>
+#include <unordered_map>
+#include <ostream>
+#include <stack>
+#include <unordered_set>
+
+using namespace std;
+
+template <typename T>
+void PrintVec(const vector<T>& vec)
+{
+	for (const auto& e : vec)
+	{
+		cout << e << " ";
+	}
+	cout << '\n';
+}
+
+
+template <typename T>
+void PrintVec(const vector<vector<T>>& vec)
+{
+	for (const auto& e1 : vec)
+	{
+		for (const auto& e2 : e1)
+		{
+			cout << e2 << " ";
+		}
+		cout << '\n';
+	}
+	cout << '\n';
+}
+
+/////////////////////////////////////////////
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int cnt;
+	int mx = -1e7;
+	int mn = 1e7;
+
+	cin >> cnt;
+	int t;
+	for (int i = 0; i < cnt; ++i)
+	{
+		cin >> t;
+
+		mx = max(mx, t);
+		mn = min(mn, t);
+
+	}
+	cout << mn << endl << mx;
+
+	return 0;
+}
+
