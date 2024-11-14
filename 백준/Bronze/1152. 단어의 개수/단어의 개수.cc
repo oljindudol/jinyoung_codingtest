57d2685699197974410e0c6589430d7b7449ed28
oljindudol <82567430+oljindudol@users.noreply.github.com>
2024-11-14 15:38:03 +0900

    [Bronze II] Title: 단어의 개수, Time: 20 ms, Memory: 5180 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.cc
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <set>
+#include <cmath>
+#include <string>
+#include <stdio.h>
+#include <unordered_map>
+#include <ostream>
+#include <stack>
+#include <unordered_set>
+#include <sstream>
+
+using namespace std;
+
+template <typename T>
+void PrintVec(const vector<T>& vec)
+{
+	for (const auto& e : vec)
+	{
+		cout << e << " ";
+	}
+	cout << '\n';
+}
+
+
+template <typename T>
+void PrintVec(const vector<vector<T>>& vec)
+{
+	for (const auto& e1 : vec)
+	{
+		for (const auto& e2 : e1)
+		{
+			cout << e2 << " ";
+		}
+		cout << '\n';
+	}
+	cout << '\n';
+}
+
+/////////////////////////////////////////////
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	string s1, s2;
+	getline(cin, s1);
+	vector<string> vs;
+	stringstream ss(s1);
+	int ret = 0;
+	while (ss >> s2)
+	{
+		++ret;
+	}
+	cout << ret;
+	return 0;
+}
+
