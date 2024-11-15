a2da6018de9cf7c35228708bc750d84a879d4777
oljindudol <82567430+oljindudol@users.noreply.github.com>
2024-11-15 16:13:50 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 40 ms, Memory: 2808 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.cc
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <set>
+#include <cmath>
+#include <string>
+#include <stdio.h>
+#include <unordered_map>
+#include <ostream>
+#include <stack>
+#include <unordered_set>
+#include <sstream>
+
+using namespace std;
+
+template <typename T>
+void PrintVec(const vector<T>& vec)
+{
+	for (const auto& e : vec)
+	{
+		cout << e << " ";
+	}
+	cout << '\n';
+}
+
+
+template <typename T>
+void PrintVec(const vector<vector<T>>& vec)
+{
+	for (const auto& e1 : vec)
+	{
+		for (const auto& e2 : e1)
+		{
+			cout << e2 << " ";
+		}
+		cout << '\n';
+	}
+	cout << '\n';
+}
+
+/////////////////////////////////////////////
+struct pos
+{
+	int x;
+	int y;
+};
+
+bool cmp(const pos& a, const pos& b)
+{
+	if (a.y == b.y)
+	{
+		return a.x < b.x;
+	}
+	return a.y < b.y;
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int cnt;
+	int x, y;
+
+	cin >> cnt;
+	vector<pos> v(cnt);
+	for (int i = 0; i < cnt; ++i)
+	{
+		cin >> v[i].x >> v[i].y;
+	}
+
+	sort(v.begin(), v.end(), cmp);
+
+	for (const auto& e : v)
+	{
+		cout << e.x << " " << e.y << " ";
+	}
+
+	return 0;
+}
