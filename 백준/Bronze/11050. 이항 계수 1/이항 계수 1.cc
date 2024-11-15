a63f61f461058779e9eb56881f5bc989914c01ea
oljindudol <82567430+oljindudol@users.noreply.github.com>
2024-11-15 11:08:07 +0900

    [Bronze I] Title: 이항 계수 1, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.cc
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <set>
+#include <cmath>
+#include <string>
+#include <stdio.h>
+#include <unordered_map>
+#include <ostream>
+#include <stack>
+#include <unordered_set>
+#include <sstream>
+
+using namespace std;
+
+template <typename T>
+void PrintVec(const vector<T>& vec)
+{
+	for (const auto& e : vec)
+	{
+		cout << e << " ";
+	}
+	cout << '\n';
+}
+
+
+template <typename T>
+void PrintVec(const vector<vector<T>>& vec)
+{
+	for (const auto& e1 : vec)
+	{
+		for (const auto& e2 : e1)
+		{
+			cout << e2 << " ";
+		}
+		cout << '\n';
+	}
+	cout << '\n';
+}
+
+/////////////////////////////////////////////
+vector<vector<int>> dp;
+int n;
+void init()
+{
+	dp.resize(n + 1);
+	dp[0].push_back(1);
+
+	for (int i = 1; i < n + 1; ++i)
+	{
+		dp[i].resize(i + 1);
+		for (int j = 0; j <= i; ++j)
+		{
+			if (0 == j)
+			{
+				dp[i][j] = dp[i - 1][0];
+				continue;
+			}
+			if (i == j)
+			{
+				dp[i][j] = dp[i - 1][j - 1];
+				continue;
+			}
+
+			dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+		}
+	}
+}
+
+
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int m;
+	cin >> n >> m;
+	init();
+	//PrintVec(dp);
+	cout << dp[n][m];
+
+	return 0;
+}
