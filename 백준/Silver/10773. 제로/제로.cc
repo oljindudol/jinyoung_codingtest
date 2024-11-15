6f6bfe36c1de33ecabc34cbdaad9366cb1644a42
oljindudol <82567430+oljindudol@users.noreply.github.com>
2024-11-15 21:32:38 +0900

    [Silver IV] Title: 제로, Time: 8 ms, Memory: 2548 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.cc
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <set>
+#include <cmath>
+#include <string>
+#include <stdio.h>
+#include <unordered_map>
+#include <ostream>
+#include <stack>
+#include <unordered_set>
+#include <sstream>
+#include <list>
+
+using namespace std;
+
+template <typename T>
+void PrintVec(const vector<T>& vec)
+{
+	for (const auto& e : vec)
+	{
+		cout << e << " ";
+	}
+	cout << '\n';
+}
+
+
+template <typename T>
+void PrintVec(const vector<vector<T>>& vec)
+{
+	for (const auto& e1 : vec)
+	{
+		for (const auto& e2 : e1)
+		{
+			cout << e2 << " ";
+		}
+		cout << '\n';
+	}
+	cout << '\n';
+}
+
+/////////////////////////////////////////////
+
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n;
+	cin >> n;
+	stack<int> s;
+	int t;
+	for (int i = 0; i < n; ++i)
+	{
+		cin >> t;
+		if (0 == t)
+		{
+			s.pop();
+		}
+		else
+		{
+			s.push(t);
+		}
+	}
+
+	int sum = 0;
+	while (false == s.empty())
+	{
+		sum += s.top();
+		s.pop();
+	}
+
+	cout << sum;
+	return 0;
+}
