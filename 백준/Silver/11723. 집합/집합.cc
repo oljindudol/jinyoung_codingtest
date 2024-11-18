1a3c17c55c593d35f07047e49f755f21389dceb2
oljindudol <82567430+oljindudol@users.noreply.github.com>
2024-11-19 08:17:31 +0900

    [Silver V] Title: 집합, Time: 544 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/11723. 집합/집합.cc
@@ -0,0 +1,135 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <set>
+#include <cmath>
+#include <string>
+#include <stdio.h>
+#include <unordered_map>
+#include <ostream>
+#include <stack>
+#include <unordered_set>
+#include <sstream>
+#include <list>
+
+using namespace std;
+
+template <typename T>
+void PrintVec(const vector<T>& vec)
+{
+	for (const auto& e : vec)
+	{
+		cout << e << " ";
+	}
+	cout << '\n';
+}
+
+
+template <typename T>
+void PrintVec(const vector<vector<T>>& vec)
+{
+	for (const auto& e1 : vec)
+	{
+		for (const auto& e2 : e1)
+		{
+			cout << e2 << " ";
+		}
+		cout << '\n';
+	}
+	cout << '\n';
+}
+
+/////////////////////////////////////////////
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	set<int> s;
+
+	int n;
+	cin >> n;
+	string str;
+	int t;
+
+	set<int> s1;
+	for (int i = 1; i < 21; ++i)
+	{
+		s1.insert(i);
+	}
+
+	while (n--)
+	{
+		cin >> str;
+
+		if ("add" == str)
+		{
+			cin >> t;
+			s.insert(t);
+			continue;
+		}
+
+		if ("remove" == str)
+		{
+			cin >> t;
+			if (s.end() != s.find(t))
+			{
+				s.erase(t);
+			}
+			continue;
+		}
+
+		if ("check" == str)
+		{
+			cin >> t;
+			if (s.end() != s.find(t))
+			{
+				//cout << 1 << " ";
+				printf("1 ");
+			}
+			else
+			{
+				//cout << 0 << " ";
+				printf("0 ");
+			}
+			continue;
+		}
+
+		if ("toggle" == str)
+		{
+			cin >> t;
+			if (s.end() != s.find(t))
+			{
+				s.erase(t);
+			}
+			else
+			{
+				s.insert(t);
+			}
+			continue;
+		}
+
+		if ("all" == str)
+		{
+			s = s1;
+			//s.clear();
+			//for (int i = 1; i < 21; ++i)
+			//{
+			//	s.insert(i);
+			//}
+			continue;
+		}
+
+		if ("empty" == str)
+		{
+			s.clear();
+			continue;
+		}
+
+	}
+
+	return 0;
+}
