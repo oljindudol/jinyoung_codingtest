f7c0296e8c02da9e100c56014b532427fa7426c0
oljindudol <82567430+oljindudol@users.noreply.github.com>
2024-11-19 09:32:02 +0900

    [Silver IV] Title: 듣보잡, Time: 48 ms, Memory: 12816 KB -BaekjoonHub

---
+++ 백준/Silver/1764. 듣보잡/듣보잡.cc
@@ -0,0 +1,99 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <set>
+#include <cmath>
+#include <string>
+#include <stdio.h>
+#include <unordered_map>
+#include <ostream>
+#include <stack>
+#include <unordered_set>
+#include <sstream>
+#include <list>
+
+using namespace std;
+
+template <typename T>
+void PrintVec(const vector<T>& vec)
+{
+	for (const auto& e : vec)
+	{
+		cout << e << " ";
+	}
+	cout << '\n';
+}
+
+
+template <typename T>
+void PrintVec(const vector<vector<T>>& vec)
+{
+	for (const auto& e1 : vec)
+	{
+		for (const auto& e2 : e1)
+		{
+			cout << e2 << " ";
+		}
+		cout << '\n';
+	}
+	cout << '\n';
+}
+
+/////////////////////////////////////////////
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	string ret = "";
+
+	unordered_map<string, int> um;
+
+	int m, n;
+	cin >> m >> n;
+	string s;
+	while (n--)
+	{
+		cin >> s;
+		um[s] = 1;
+	}
+
+	while (m--)
+	{
+		cin >> s;
+		if (1 == um[s])
+		{
+			um[s] = 2;
+		}
+		else
+		{
+			um[s] = 1;
+		}
+	}
+
+	vector<string> sret;
+	for (const auto& e : um)
+	{
+		if (2 == e.second)
+		{
+			sret.push_back(e.first);
+		}
+	}
+
+	ret += to_string(sret.size());
+	ret += " ";
+
+	sort(sret.begin(), sret.end());
+
+	for (int i = 0; i < sret.size(); ++i)
+	{
+		ret += " ";
+		ret += sret[i];
+	}
+	cout << ret;
+
+	return 0;
+}
