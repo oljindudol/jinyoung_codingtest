a0e697ef5db5b0a3343a0ace1e3c5c7272e0e712
oljindudol <82567430+oljindudol@users.noreply.github.com>
2024-11-21 11:05:30 +0900

    [Silver II] Title: 최소 힙, Time: 16 ms, Memory: 2920 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.cc
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <set>
+#include <cmath>
+#include <string>
+#include <stdio.h>
+#include <unordered_map>
+#include <ostream>
+#include <stack>
+#include <unordered_set>
+#include <sstream>
+#include <list>
+
+using namespace std;
+
+template <typename T>
+void PrintVec(const vector<T>& vec)
+{
+	for (const auto& e : vec)
+	{
+		cout << e << " ";
+	}
+	cout << '\n';
+}
+
+
+template <typename T>
+void PrintVec(const vector<vector<T>>& vec)
+{
+	for (const auto& e1 : vec)
+	{
+		for (const auto& e2 : e1)
+		{
+			cout << e2 << " ";
+		}
+		cout << '\n';
+	}
+	cout << '\n';
+}
+
+/////////////////////////////////////////////
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+
+	int n;
+	cin >> n;
+
+	int t;
+	string ret = "";
+	priority_queue<int, vector<int>, greater<int>> pq;
+	while (n--)
+	{
+		cin >> t;
+
+		if (0 == t)
+		{
+			if (true == pq.empty())
+			{
+				ret += '0';
+				ret += " ";
+			}
+			else
+			{
+				ret += to_string(pq.top());
+				ret += " ";
+				pq.pop();
+			}
+		}
+		else
+		{
+			pq.push(t);
+		}
+	}
+
+	cout << ret;
+	return 0;
+}
