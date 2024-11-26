72d146af0d9f16d3de7267ba532be2088af82114
oljindudol <82567430+oljindudol@users.noreply.github.com>
2024-11-27 08:45:24 +0900

    [Silver I] Title: 경로 찾기, Time: 4 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.cc
@@ -0,0 +1,98 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <set>
+#include <cmath>
+#include <string>
+#include <stdio.h>
+#include <unordered_map>
+#include <ostream>
+#include <stack>
+#include <unordered_set>
+#include <sstream>
+#include <list>
+#include <thread>
+#include <omp.h>
+
+using namespace std;
+
+template <typename T>
+void PrintVec(const vector<T>& vec)
+{
+	for (const auto& e : vec)
+	{
+		cout << e << " ";
+	}
+	cout << '\n';
+}
+
+
+template <typename T>
+void PrintVec(const vector<vector<T>>& vec)
+{
+	for (const auto& e1 : vec)
+	{
+		for (const auto& e2 : e1)
+		{
+			cout << e2 << " ";
+		}
+		cout << '\n';
+	}
+	cout << '\n';
+}
+
+/////////////////////////////////////////////
+int n;
+vector<vector<int>> map;
+
+void dfs(const int from, int to, vector<int>& visited, bool first)
+{
+	if (false == first)
+	{
+		visited[to] = 1;
+		map[from][to] = 1;
+	}
+	else
+	{
+		first = false;
+	}
+
+	for (int i = 0; i < n; ++i)
+	{
+		if (1 == map[to][i] && 0 == visited[i])
+		{
+			dfs(from, i, visited, first);
+		}
+	}
+}
+
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> n;
+
+	map.resize(n, vector<int>(n));
+
+	for (int row = 0; row < n; ++row)
+	{
+		for (int col = 0; col < n; ++col)
+		{
+			cin >> map[row][col];
+		}
+	}
+
+	for (int i = 0; i < n; ++i)
+	{
+		vector<int> visited(n);
+		dfs(i, i, visited, true);
+	}
+
+	PrintVec(map);
+
+	return 0;
+}
