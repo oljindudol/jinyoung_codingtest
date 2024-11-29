3ecd00bed4ec6ef7a79ce37f67a065bcef357b8e
oljindudol <82567430+oljindudol@users.noreply.github.com>
2024-11-29 10:24:01 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 44 ms, Memory: 12492 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.cc
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.cc
@@ -3,51 +3,87 @@
 #include <algorithm>
 #include <queue>
 #include <set>
-#include <math.h>
+#include <cmath>
 #include <string>
 #include <stdio.h>
 #include <unordered_map>
+#include <ostream>
+#include <stack>
+#include <unordered_set>
+#include <sstream>
+#include <list>
+#include <thread>
+#include <omp.h>
 
 using namespace std;
 
-int n;
-vector<int> dp;
-vector<vector<int>> map;
-
-void dfs(int cur)
+template <typename T>
+void PrintVec(const vector<T>& vec)
 {
-	for (int i = 0; i < map[cur].size(); ++i)
+	for (const auto& e : vec)
 	{
-		if (-1 == dp[map[cur][i]])
+		cout << e << " ";
+	}
+	cout << '\n';
+}
+
+
+template <typename T>
+void PrintVec(const vector<vector<T>>& vec)
+{
+	for (const auto& e1 : vec)
+	{
+		for (const auto& e2 : e1)
 		{
-			dp[map[cur][i]] = cur;
-			dfs(map[cur][i]);
+			cout << e2 << " ";
 		}
+		cout << '\n';
+	}
+	cout << '\n';
+}
+
+/////////////////////////////////////////////
+int n;
+vector<int> parents;
+vector<vector<int>> edges;
+void dfs(int from)
+{
+	for (const auto& to : edges[from])
+	{
+		if (0 != parents[to])
+		{
+			continue;
+		}
+		parents[to] = from;
+		dfs(to);
 	}
 }
 
 
 int main()
 {
-	cin >> n;
-	dp = vector<int>(n + 1, -1);
-	map = vector<vector<int>>(n + 1);
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
-	int a, b;
+	cin >> n;
+	parents.resize(n + 1);
+	edges.resize(n + 1);
+
+	int t1, t2;
 	for (int i = 0; i < n - 1; ++i)
 	{
-		cin >> a >> b;
-		map[a].push_back(b);
-		map[b].push_back(a);
+		cin >> t1 >> t2;
+		edges[t1].push_back(t2);
+		edges[t2].push_back(t1);
 	}
 
+	parents[1] = 1;
 	dfs(1);
 
 	for (int i = 2; i < n + 1; ++i)
 	{
-		printf("%d ", dp[i]);
-		//cout << dp[i] << endl;
+		cout << parents[i] << " ";
 	}
-
 	return 0;
 }
