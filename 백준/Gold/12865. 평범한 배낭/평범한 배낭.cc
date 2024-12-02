a0ca52002bf3ea3a6a7a8e268c62545bd123c35c
oljindudol <82567430+oljindudol@users.noreply.github.com>
2024-12-02 09:11:06 +0900

    [Gold V] Title: 평범한 배낭, Time: 8 ms, Memory: 2924 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -3,51 +3,81 @@
 #include <algorithm>
 #include <queue>
 #include <set>
-#include <math.h>
+#include <cmath>
+#include <string>
+#include <stdio.h>
+#include <unordered_map>
+#include <ostream>
+#include <stack>
+#include <unordered_set>
+#include <sstream>
+#include <list>
+#include <thread>
+#include <omp.h>
+#include <mutex>
+
 using namespace std;
 
+template <typename T>
+void PrintVec(const vector<T>& vec)
+{
+	for (const auto& e : vec)
+	{
+		cout << e << " ";
+	}
+	cout << '\n';
+}
+
+
+template <typename T>
+void PrintVec(const vector<vector<T>>& vec)
+{
+	for (const auto& e1 : vec)
+	{
+		for (const auto& e2 : e1)
+		{
+			cout << e2 << " ";
+		}
+		cout << '\n';
+	}
+	cout << '\n';
+}
+
+/////////////////////////////////////////////
 struct item
 {
-	int w;
 	int v;
+	int w;
 };
-
-
 
 int main()
 {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
-
+	cout.tie(NULL);
 	int n, k;
+
 	cin >> n >> k;
 
-	vector<item> items;
+	vector<int> dp(k + 1);
+
+	vector<item> items(n);
 	for (int i = 0; i < n; ++i)
 	{
-		int w, v;
-		cin >> w >> v;
-		items.push_back({ w,v });
+		cin >> items[i].w >> items[i].v;
 	}
 
-	vector<int> dp(k + 1, 0);
-
-	for (int i = 0; i < n; ++i)
+	for (const auto i : items)
 	{
 		auto pdp = dp;
-
-		for (int j = 1; j < k + 1; ++j)
+		for (int w = i.w; w < k + 1; ++w)
 		{
-			//공간이 남아있으면
-			if (j >= items[i].w)
-			{
-				int newweight = j - items[i].w;
-				dp[j] = max(pdp[j], pdp[newweight] + items[i].v);
-			}
+			int neww = w - i.w;
+			int newv = pdp[neww] + i.v;
+			dp[w] = max(pdp[w], newv);
 		}
 	}
 
 	cout << dp[k];
-
 	return 0;
 }
