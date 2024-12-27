826542f6c413a6e906fced4180150ce6ffe26998
oljindudol <82567430+oljindudol@users.noreply.github.com>
2024-12-27 11:11:28 +0900

    [level 1] Title: 모의고사, Time: 0.32 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.cpp
+++ 프로그래머스/1/42840. 모의고사/모의고사.cpp
@@ -1,49 +1,37 @@
 #include <string>
 #include <vector>
-#include <stdio.h>
+#include <algorithm>
 
 using namespace std;
 
 vector<int> solution(vector<int> answers) {
     vector<int> answer;
+    vector<vector<int>> numss(3);
+    vector<int> sols(3,0);
+    numss[0] = { 1,2,3,4,5};
+    numss[1] = {2,1,2,3,2,4,2,5};
+    numss[2] = {3,3,1,1,2,2,4,4,5,5};
     
-    //12345
-    vector<int> num1 = {1,2,3,4,5};
-    
-    // 21232425
-    vector<int> num2 = {2,1,2,3,2,4,2,5};
-    
-    //33 11 22 44 55
-    vector<int> num3 = {3,3,1,1,2,2,4,4,5,5};
-    
-    int cor1=0;
-    int cor2=0;
-    int cor3=0;
-    
-    for(size_t i= 0 ; i<answers.size() ;++i)
+    for(int i = 0 ; i < answers.size();++i)
     {
-        if(answers[i] == num1[i % num1.size()])
-            ++cor1;
-        if(answers[i] == num2[i % num2.size()])
-            ++cor2;
-        if(answers[i] == num3[i % num3.size()])
-            ++cor3;
+        for(int j =0 ; j < 3; ++j)
+        {
+            if(answers[i] ==numss[j][i%numss[j].size()])
+            {
+                ++sols[j];
+            }
+        }
     }
     
+    int maxsol = *max_element(sols.begin(),sols.end()); 
     
-    printf("%d\n",&cor1);
-    printf("%d\n",&cor2);
-    printf("%d\n",&cor3);
-    
-    int max = 0;
-    max = max < cor1 ? cor1:max;
-    max = max < cor2 ? cor2:max;
-    max = max < cor3 ? cor3:max;
-    
-    if(cor1 == max)  answer.push_back(1);
-    if(cor2 == max)  answer.push_back(2);
-    if(cor3 == max)  answer.push_back(3);
-    
+    for(int i =0 ; i <3;++i)
+    {
+        if(maxsol == sols[i])
+        {
+            answer.push_back(i+1);
+        }
+    }
     
     return answer;
 }
