b0a2ff3ec7429003e1ba0f98c2be7b317356f5af
oljindudol <82567430+oljindudol@users.noreply.github.com>
2025-01-02 10:09:26 +0900

    [level 3] Title: 정수 삼각형, Time: 0.41 ms, Memory: 8.3 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.cpp
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.cpp
@@ -1,62 +1,36 @@
 #include <string>
 #include <vector>
+#include <algorithm>
 #include <iostream>
 
 using namespace std;
 
-int maxel=-1;
-vector<int> dp;
-int maxdepth;
-
-void dfs(const vector<vector<int>>& triangle,int depth)
-{
-    if(maxdepth < depth)
-    {
-        return;
-    }
-    
-    vector<int> pdp;
-    for(int i = 0 ; i < depth;++i)
-    {
-        pdp.push_back(dp[i]);
-    }
-    
-    //depth만큼돈다
-    for(int i = 0 ; i < depth;++i)
-    {
-        if(0 == i)
-        {
-            dp[i] = pdp[i] + triangle[depth-1][i];
-            continue;
-        }
-        else if(depth-1 == i)
-        {
-            dp[i] = pdp[i-1] +triangle[depth-1][i]; 
-            continue;
-        }
-        else
-        {
-            dp[i] = max(pdp[i] + triangle[depth-1][i] ,pdp[i-1] +triangle[depth-1][i] );
-            continue;
-        }
-    }
-    
-    dfs(triangle,depth+1);
-    
-}
-
 int solution(vector<vector<int>> triangle) {
     int answer = 0;
-    maxdepth = triangle.size();
+    int sz = triangle.size();
     
-    
-    dp = vector<int>(maxdepth,0);
-    dp[0] = triangle[0][0];
-    dfs(triangle,2);
-    
-    for(int i = 0 ; i < maxdepth; ++i)
+    for(int row = 1 ;row<sz;++row)
     {
-        answer = max(answer,dp[i]);
+        for(int col = 0;col<=row;++col)
+        {
+            if(0==col)
+            {
+                triangle[row][col] += triangle[row-1][col];
+            }
+            else if(row==col)
+            {
+                triangle[row][col] += triangle[row-1][col-1];
+            }
+            else
+            {
+                triangle[row][col] += max(triangle[row-1][col-1],triangle[row-1][col]);
+            }
+            // cout<< triangle[row][col]<<' ';
+        }
+        // cout<<'\n';
     }
+    
+    answer = *max_element(triangle[sz-1].begin(),triangle[sz-1].end());
+    
     return answer;
 }
