21960993fc41c4eb6dbd35e7617a42e7bdf6509e
oljindudol <82567430+oljindudol@users.noreply.github.com>
2025-01-10 08:13:56 +0900

    [Bronze II] Title: 벌집, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2292. 벌집/벌집.cc
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <set>
+#include <cmath>
+#include <string>
+#include <stdio.h>
+#include <unordered_map>
+#include <ostream>
+#include <stack>
+#include <unordered_set>
+#include <sstream>
+#include <list>
+#include <thread>
+#include <omp.h>
+#include <mutex>
+#include <assert.h>
+
+using namespace std;
+
+template <typename T>
+void PrintVec(const vector<T>& vec)
+{
+	for (const auto& e : vec)
+	{
+		cout << e << " ";
+	}
+	cout << '\n';
+}
+
+
+template <typename T>
+void PrintVec(const vector<vector<T>>& vec)
+{
+	for (const auto& e1 : vec)
+	{
+		for (const auto& e2 : e1)
+		{
+			cout << e2 << " ";
+		}
+		cout << '\n';
+	}
+	cout << '\n';
+}
+
+/////////////////////////////////////////////
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n;
+	cin >> n;
+	int ret = 1;
+	if (1 == n)
+	{
+		cout << 1;
+		return 0;
+	}
+	--n;
+	int cur = 6;
+	do
+	{
+		n -= cur;
+		cur += 6;
+		++ret;
+	} while (0 < n);
+
+	cout << ret;
+	return 0;
+}
